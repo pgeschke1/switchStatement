e11f40c2fbd00be674e73c8f6c0c172c48067756
pgeschke1 <pgeschke@students.mchenry.edu>
2018-01-28 15:55:55 -0600

    Initial commit

---
+++ src/com/paulgeschke/Main.java
@@ -0,0 +1,50 @@
+package com.paulgeschke;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number 1-10: ");
+        number = scanner.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Uno");
+                break;
+            case 2:
+                System.out.println("Dos");
+                break;
+            case 3:
+                System.out.println("Tres");
+                break;
+            case 4:
+                System.out.println("Cuatro");
+                break;
+            case 5:
+                System.out.println("Cinco");
+                break;
+            case 6:
+                System.out.println("Seis");
+                break;
+            case 7:
+                System.out.println("Siete");
+                break;
+            case 8:
+                System.out.println("Ocho");
+                break;
+            case 9:
+                System.out.println("Nueve");
+                break;
+            case 10:
+                System.out.println("Diez");
+                break;
+            default:
+                System.out.println("Not a valid number.");
+        }
+
+    }
+}
